5fc35d9e7d8c8c9e1c5f251b65f2a190b655c557
rmf89685 <rmf89685@marymount.edu>
2024-10-15 01:19:00 -0400

    get_nvd_data.py (working...I think) EOD 20241015 rmflo

--- sue_data/get_nvd_data.py
+++ sue_data/get_nvd_data.py
@@ -38,44 +38,51 @@ with open('./sue_data/json_data/detected_vulnerabilities.json') as data_file:
                 current_metrics = vulnerability.metrics # create dictionary of current vulnerability metrics
 
                 # combine detected vulnerabilities list with score component data into combined list
-                if hasattr(current_metrics, "cvssMetricV31") or hasattr(current_metrics, "cvssMetricV30"):
-                    # set highest CVSS version for current vulnerability
-                    if hasattr(current_metrics, "cvssMetricV31"):
-                        cvss_version = "cvssMetricV31"
-                    else:
-                        cvss_version = "cvssMetric30"
+                if hasattr(current_metrics, "cvssMetricV31"):
+                    # assign score component values
+                    combined_list[index]["cvss_version"] = 3.1
+                    combined_list[index]["vector_string"] = vulnerability.v31vector
+                    combined_list[index]["impact_conf"] = vulnerability.v31confidentialityImpact 
+                    combined_list[index]["impact_integ"] = vulnerability.v31integrityImpact 
+                    combined_list[index]["impact_avail"] = vulnerability.v31availabilityImpact 
+                    combined_list[index]["scope_changed"] = vulnerability.v31scope 
+                    combined_list[index]["attack_vector"] = vulnerability.v31attackVector 
+                    combined_list[index]["attack_complexity"] = vulnerability.v31attackComplexity 
+                    combined_list[index]["privilege_required"] = vulnerability.v31privilegesRequired 
+                    combined_list[index]["user_interaction"] = vulnerability.v31userInteraction 
+                    # combined_list[index]["exploit_code_maturity"] = vulnerability.
+                    # combined_list[index]["remediation_level"] = vulnerability.
+                    # combined_list[index]["report_confidence"] = vulnerability.
+                
+                elif hasattr(current_metrics, "cvssMetricV30"):
+                    # assign score component values
+                    combined_list[index]["cvss_version"] = 3.0
+                    combined_list[index]["vector_string"] = vulnerability.v30vector
+                    combined_list[index]["impact_conf"] = vulnerability.v31confidentialityImpact
+                    combined_list[index]["impact_integ"] = vulnerability.v31integrityImpact
+                    combined_list[index]["impact_avail"] = vulnerability.v31availabilityImpact
+                    combined_list[index]["scope_changed"] = vulnerability.v31scope
+                    combined_list[index]["attack_vector"] = vulnerability.v31attackVector
+                    combined_list[index]["attack_complexity"] = vulnerability.v31attackComplexity
+                    combined_list[index]["privilege_required"] = vulnerability.v31privilegesRequired
+                    combined_list[index]["user_interaction"] = vulnerability.v31userInteraction
+                    # combined_list[index]["exploit_code_maturity"] = vulnerability.
+                    # combined_list[index]["remediation_level"] = vulnerability.
+                    # combined_list[index]["report_confidence"] = vulnerability.
 
+                else:  
                     # assign score component values
-                    combined_list[index]["cvss_version"] = current_metrics[cvss_version][cvssData].get("version")
-                    combined_list[index]["vector_string"] = current_metrics[cvss_version][cvssData].get("vectorString")
-                    combined_list[index]["impact_conf"] = current_metrics[cvss_version][cvssData].get("confidentialityImpact")
-                    combined_list[index]["impact_integ"] = current_metrics[cvss_version][cvssData].get("integrityImpact")
-                    combined_list[index]["impact_avail"] = vulnerability.metrics[cvss_version].get("availabilityImpact")
-                    combined_list[index]["scope_changed"] = current_metrics[cvss_version][cvssData].get("scope")
-                    combined_list[index]["attack_vector"] = current_metrics[cvss_version][cvssData].get("attackVector")
-                    combined_list[index]["attack_complexity"] = current_metrics[cvss_version][cvssData].get("attackComplexity")
-                    combined_list[index]["privilege_required"] = current_metrics[cvss_version][cvssData].get("privilegesRequired")
-                    combined_list[index]["user_interaction"] = current_metrics[cvss_version][cvssData].get("userInteraction")
-                    # combined_list[index]["exploit_code_maturity"] = current_metrics[cvss_version][cvssData].get("confidentialityImpact")
-                    # combined_list[index]["remediation_level"] = current_metrics[cvss_version][cvssData].get("confidentialityImpact")
-                    # combined_list[index]["report_confidence"] = current_metrics[cvss_version][cvssData].get("confidentialityImpact")
-                
-                else:
-                    # set highest CVSS version for current vulnerability
-                    cvss_version = "cvssMetricV2"
-                
-                    # assign score component values
-                    combined_list[index]["cvss_version"] = current_metrics[cvss_version][cvssData].get("version")
-                    combined_list[index]["vector_string"] = current_metrics[cvss_version][cvssData].get("vectorString")
-                    combined_list[index]["impact_conf"] = current_metrics[cvss_version][cvssData].get("confidentialityImpact")
-                    combined_list[index]["impact_integ"] = current_metrics[cvss_version][cvssData].get("integrityImpact")
-                    combined_list[index]["impact_avail"] = vulnerability.metrics[cvss_version].get("availabilityImpact")
-                    # combined_list[index]["scope_changed"] = current_metrics[cvss_version][cvssData].get("scope")
-                    combined_list[index]["attack_vector"] = current_metrics[cvss_version][cvssData].get("accessVector")
-                    combined_list[index]["attack_complexity"] = current_metrics[cvss_version][cvssData].get("accessComplexity")
-                    combined_list[index]["privilege_required"] = current_metrics[cvss_version][cvssData].get("authentication")
-                    combined_list[index]["user_interaction"] = current_metrics[cvss_version][cvssData].get("userInteractionRequired")
-                    # combined_list[index]["exploit_code_maturity"] = current_metrics[cvss_version][cvssData].get("confidentialityImpact")
-                    # combined_list[index]["remediation_level"] = current_metrics[cvss_version][cvssData].get("confidentialityImpact")
-                    # combined_list[index]["report_confidence"] = current_metrics[cvss_version][cvssData].get("confidentialityImpact")
-            # print(combined_list)
+                    combined_list[index]["cvss_version"] = 2.0
+                    combined_list[index]["vector_string"] = vulnerability.v2vector
+                    combined_list[index]["impact_conf"] = vulnerability.v2confidentialityImpact
+                    combined_list[index]["impact_integ"] = vulnerability.v2integrityImpact
+                    combined_list[index]["impact_avail"] = vulnerability.v2availabilityImpact
+                    # combined_list[index]["scope_changed"] = vulnerability.
+                    combined_list[index]["attack_vector"] = vulnerability.v2accessVector
+                    combined_list[index]["attack_complexity"] = vulnerability.v2accessComplexity 
+                    # combined_list[index]["privilege_required"] = vulnerability.
+                    # combined_list[index]["user_interaction"] = vulnerability.
+                    # combined_list[index]["exploit_code_maturity"] = vulnerability.
+                    # combined_list[index]["remediation_level"] = vulnerability.
+                    # combined_list[index]["report_confidence"] = vulnerability.
+            # # print(combined_list)
