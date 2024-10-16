1dbedb8fe9a0478a2b4a72865232a755aef91e8f
rmf89685 <rmf89685@marymount.edu>
2024-10-16 10:49:24 -0400

    get_nvd_data.py BROKEN timing out BOD 20241016

--- sue_data/get_nvd_data.py
+++ sue_data/get_nvd_data.py
@@ -5,84 +5,91 @@ import sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-install('nvdlib') # install nvdlib
+install('nvdlib')
 
 import json
 import nvdlib
 
-# Read the NIST NVD API key from the file
-with open('../.aws/nvd_api_key.txt') as key_file:
-    nvd_api_key = key_file.read() # NIST NVD API key file location
 vulnerabilities_list = list() # create an empty list to use for detected vulnerabilities data
-combined_list = list() # create an empy list to use for combined nodes with vulnerabilities
+combined_list = list() # create an empy list to use for combined nodes with vulnerability data
 
-# pull NVD data for all detected vulnerabilities
-with open('./sue_data/json_data/detected_vulnerabilities.json') as data_file:    
-    data = json.load(data_file) # load data from detected vulnerabilities json file
-    cves =set([item["CVE Number"] for item in data]) # create distinct list of CVE's detected
+def main(nvd_api_key_file_location, vulnerabilities_detected_file_location):
+    # read the NIST NVD API key from the file
+    with open(nvd_api_key_file_location) as key_file:
+        nvd_api_key = key_file.read() # read API key file to variable
+    # call function to extract distinct CVE numbers from detected vulnerabilities file
+    get_detected_vulnerabilities(nvd_api_key, vulnerabilities_detected_file_location)
+    build_vulnerability_data()
 
-    # iterate through CVE's detected and pull data for each CVE
-    for cve_id in cves:
-        cve_search = nvdlib.searchCVE(cveId=cve_id, key=nvd_api_key, delay=1)[0] # search current CVE
-        vulnerabilities_list.append(cve_search) # append current CVE data to vulnerabilities list
+def get_detected_vulnerabilities(nvd_api_key, vulnerabilities_detected_file_location):
+    global combined_list
+    # pull NVD data for all detected vulnerabilities
+    with open(vulnerabilities_detected_file_location) as data_file:   
+        data = json.load(data_file) # load data from detected vulnerabilities json file
+        combined_list = data # load combined list with detected vulnerabilities
+        cves =set([item["CVE Number"] for item in data]) # create distinct list of CVE's detected
 
-    combined_list = data # create a copy of data as combined_list
+        # iterate through CVE's detected and pull data for each CVE
+        for cve_id in cves:
+            cve_search = nvdlib.searchCVE(cveId=cve_id, key=nvd_api_key, delay=10)[0] # search current CVE
+            # cve_search = nvdlib.searchCVE(cveId=cve_id)[0]
+            vulnerabilities_list.append(cve_search) # append current CVE data to vulnerabilities list
 
-    # combined vulnerabilities discovered and required score components to combined list
+def build_vulnerability_data():
+    global combined_list
+    # combined vulnerabilities discovered and CVE score components to combined list
     for index in range(len(combined_list)):
-        current_cve = combined_list[index].get("CVE Number")
+        current_cve = combined_list[index].get("CVE Number")     
         
-        # assign value for confidentiality impact
-        for vulnerability in vulnerabilities_list:
-            if vulnerability.id == current_cve:
-                current_metrics = vulnerability.metrics # create dictionary of current vulnerability metrics
+        # assign score component values
+        combined_list[index]["cvss_version"] = append_cve_data(current_cve, "version", "version")
+        combined_list[index]["vector_string"] = append_cve_data(current_cve, "vectorString", "vectorString")
+        combined_list[index]["attack_vector"] = append_cve_data(current_cve, "attackVector", "accessVector")
+        combined_list[index]["attack_complexity"] = append_cve_data(current_cve, "attackComplexity", "accessComplexity")
+        combined_list[index]["privilege_required"] = append_cve_data(current_cve, "privilegesRequired", "authentication")
+        combined_list[index]["user_interaction"] = append_cve_data(current_cve, "userInteraction", "userInteractionRequired")
+        combined_list[index]["scope_changed"] = append_cve_data(current_cve, "scope", "NA")
+        combined_list[index]["impact_conf"] = append_cve_data(current_cve, "confidentialityImpact", "confidentialityImpact")
+        combined_list[index]["impact_integ"] = append_cve_data(current_cve, "integrityImpact", "integrityImpact")
+        combined_list[index]["impact_avail"] = append_cve_data(current_cve, "availabilityImpact", "availabilityImpact")
+        combined_list[index]["base_score"] = append_cve_data(current_cve, "baseScore", "baseScore")
+        combined_list[index]["base_severity"] = append_cve_data(current_cve, "baseSeverity", "baseSeverity")
+        combined_list[index]["exploitability_score"] = append_cve_data(current_cve, "exploitabilityScore", "exploitabilityScore")
+        combined_list[index]["impact_score"] = append_cve_data(current_cve, "impactScore", "impactScore")
+        # combined_list[index]["exploit_code_maturity"] = vulnerability.
+        # combined_list[index]["remediation_level"] = vulnerability.
+        # combined_list[index]["report_confidence"] = vulnerability.
+        
+def append_cve_data(current_cve, v3score_component, v2score_component):
+    global vulnerabilities_list
 
-                # combine detected vulnerabilities list with score component data into combined list
-                if hasattr(vulnerability.metrics, "cvssMetricV31"):
-                    # assign score component values
-                    combined_list[index]["cvss_version"] = 3.1
-                    combined_list[index]["vector_string"] = vulnerability.v31vector
-                    combined_list[index]["impact_conf"] = vulnerability.v31confidentialityImpact 
-                    combined_list[index]["impact_integ"] = vulnerability.v31integrityImpact 
-                    combined_list[index]["impact_avail"] = vulnerability.v31availabilityImpact 
-                    combined_list[index]["scope_changed"] = vulnerability.v31scope 
-                    combined_list[index]["attack_vector"] = vulnerability.v31attackVector 
-                    combined_list[index]["attack_complexity"] = vulnerability.v31attackComplexity 
-                    combined_list[index]["privilege_required"] = vulnerability.v31privilegesRequired 
-                    combined_list[index]["user_interaction"] = vulnerability.v31userInteraction 
-                    # combined_list[index]["exploit_code_maturity"] = vulnerability.
-                    # combined_list[index]["remediation_level"] = vulnerability.
-                    # combined_list[index]["report_confidence"] = vulnerability.
-                
-                elif hasattr(vulnerability.metrics, "cvssMetricV30"):
-                    # assign score component values
-                    combined_list[index]["cvss_version"] = 3.0
-                    combined_list[index]["vector_string"] = vulnerability.v30vector
-                    combined_list[index]["impact_conf"] = vulnerability.v31confidentialityImpact
-                    combined_list[index]["impact_integ"] = vulnerability.v31integrityImpact
-                    combined_list[index]["impact_avail"] = vulnerability.v31availabilityImpact
-                    combined_list[index]["scope_changed"] = vulnerability.v31scope
-                    combined_list[index]["attack_vector"] = vulnerability.v31attackVector
-                    combined_list[index]["attack_complexity"] = vulnerability.v31attackComplexity
-                    combined_list[index]["privilege_required"] = vulnerability.v31privilegesRequired
-                    combined_list[index]["user_interaction"] = vulnerability.v31userInteraction
-                    # combined_list[index]["exploit_code_maturity"] = vulnerability.
-                    # combined_list[index]["remediation_level"] = vulnerability.
-                    # combined_list[index]["report_confidence"] = vulnerability.
+    for vulnerability in vulnerabilities_list:
+        if vulnerability.id == current_cve:
+            metrics = getattr(vulnerability, 'metrics')
 
-                else:  
-                    # assign score component values
-                    combined_list[index]["cvss_version"] = 2.0
-                    combined_list[index]["vector_string"] = vulnerability.v2vector
-                    combined_list[index]["impact_conf"] = vulnerability.v2confidentialityImpact
-                    combined_list[index]["impact_integ"] = vulnerability.v2integrityImpact
-                    combined_list[index]["impact_avail"] = vulnerability.v2availabilityImpact
-                    # combined_list[index]["scope_changed"] = vulnerability.
-                    combined_list[index]["attack_vector"] = vulnerability.v2accessVector
-                    combined_list[index]["attack_complexity"] = vulnerability.v2accessComplexity 
-                    # combined_list[index]["privilege_required"] = vulnerability.
-                    # combined_list[index]["user_interaction"] = vulnerability.
-                    # combined_list[index]["exploit_code_maturity"] = vulnerability.
-                    # combined_list[index]["remediation_level"] = vulnerability.
-                    # combined_list[index]["report_confidence"] = vulnerability.
-            print(combined_list)
+            if metrics:  # Check if metrics attribute exists and is not None
+                first_metric = next((m for m in metrics if hasattr(m, 'cvssData')), None)
+                if first_metric:
+                    if hasattr(first_metric.cvssData, v3score_component):
+                        return getattr(first_metric.cvssData, v3score_component)
+                    elif hasattr(first_metric.cvssData, v2score_component):
+                        return getattr(first_metric.cvssData, v2score_component)
+                    else:
+                        return "NO_METRIC"
+                else:
+                    return "ERROR: No cvssData found in metrics"
+            else:
+                return "ERROR: No metrics attribute found in vulnerability"
+
+# set file locations if this code is run directly/not called from another script
+if __name__ == "__main__":
+    nvd_api_key_file_location = '../.aws/nvd_api_key.txt' # sets NIST NVD API key location
+    vulnerabilities_detected_file_location = './sue_data/json_data/detected_vulnerabilities.json' # sets vulnerabilities detected file location
+    main(nvd_api_key_file_location, vulnerabilities_detected_file_location) # passes both variables to main() to run script
+    print("************************BEGIN DETECTED VULNERABILITIES SUMMARY************************")
+    print()
+    for entry in combined_list:
+        print(entry)
+        print()
+        print("**********************************************************************************")
+        print()
