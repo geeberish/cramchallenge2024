5aeb81f700cb531ff5764f7bae6feb0487800153
mattpenn2 <mattpenn7@gmail.com>
2024-10-18 02:06:33 -0400

    PPP Returns

--- modifiedscore.py
+++ modifiedscore.py
@@ -1,5 +1,8 @@
 import math
+from LLamaPPP import get_security_analysis
 
+test = get_security_analysis('frameworks/CSF_Best_Prac_KV.json','sue_data/json_data/summaries.json')
+print(test)
 # import cvss base average score from the nist database, as well as variables needed for criticality
 
 # import 3 p's from llama api
