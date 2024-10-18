36d0d4afe9c20499f22eaba5cb2d41be1bccfdf0
rmf89685 <rmf89685@marymount.edu>
2024-10-18 09:53:38 -0400

    moved Richard's .py files to main directory rmflo

---
+++ set_max_node_criticalites.py
@@ -0,0 +1,10 @@
+import math
+
+def main(combined_vulnerabilities_data):
+  for vulnerability in combined_vulnerabilities_data:
+    max_criticality = 0 # initiate max_criticality to store highest critical function value per node
+  print("TBD")
+
+
+if __name__ == "__main__":
+  print("TBD")
